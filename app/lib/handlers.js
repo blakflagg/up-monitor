198a4092d335b59a223b2a6f4508d101739b9890
blakflagg <dduffy2@outlook.com>
2018-09-26 19:27:25 -0700

    user can create new account and login to the account

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -49,6 +49,71 @@ handlers.index = (data, callback) => {
   }
 };
 
+// Create Account
+handlers.accountCreate = (data,callback) =>{
+  //Reject any request that isn't a GET
+  if (data.method == 'get') {
+
+    //Prepare data for interpolation
+    let templateData = {
+      'head.title': 'Create An Account',
+      'head.description': 'Sign up here',
+      'body.class': 'accountCreate'
+    };
+
+    //Read in the index template as a string
+    helpers.getTemplate('accountCreate', templateData, (err, str) => {
+      if (!err && str) {
+        //Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, str) => {
+          if (!err && str) {
+            //return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
+// Create Account
+handlers.sessionCreate = (data,callback) =>{
+  //Reject any request that isn't a GET
+  if (data.method == 'get') {
+
+    //Prepare data for interpolation
+    let templateData = {
+      'head.title': 'Login To Your Account',
+      'head.description': 'Please enter  your phone number and password to access your account',
+      'body.class': 'sessionCreate'
+    };
+
+    //Read in the index template as a string
+    helpers.getTemplate('sessionCreate', templateData, (err, str) => {
+      if (!err && str) {
+        //Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, str) => {
+          if (!err && str) {
+            //return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
+
 //Favicon
 handlers.favicon = (data, callback) => {
   //Reject an request that is not GET
