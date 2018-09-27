058c159eda4379e9071c4ecd9a49e917eebb6b70
blakflagg <dduffy2@outlook.com>
2018-09-26 22:02:19 -0700

    user dashboard (shows all checks, allow new check creation)

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -242,6 +242,39 @@ handlers.checksCreate = (data,callback) =>{
     callback(405, undefined, 'html');
   }
 };
+// Dashboard (view all checks)
+handlers.checksList = (data,callback) =>{
+  //Reject any request that isn't a GET
+  if (data.method == 'get') {
+
+    //Prepare data for interpolation
+    let templateData = {
+      'head.title': 'Dashboard',
+      'body.class': 'checksList'
+    };
+
+    //Read in the index template as a string
+    helpers.getTemplate('checksList', templateData, (err, str) => {
+      if (!err && str) {
+        //Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, str) => {
+          if (!err && str) {
+            //return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
+
+
 //Favicon
 handlers.favicon = (data, callback) => {
   //Reject an request that is not GET
