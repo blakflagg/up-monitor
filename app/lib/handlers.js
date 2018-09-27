3dcd004fa0f968415b1e57b700fac8d5310ade47
blakflagg <dduffy2@outlook.com>
2018-09-26 21:02:37 -0700

    added edit account page and wired up functions

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -147,6 +147,37 @@ handlers.sessionDeleted = (data,callback) =>{
   }
 };
 
+// Edit your account
+handlers.accountEdit = (data,callback) =>{
+  //Reject any request that isn't a GET
+  if (data.method == 'get') {
+
+    //Prepare data for interpolation
+    let templateData = {
+      'head.title': 'Account Settings',
+      'body.class': 'accountEdit'
+    };
+
+    //Read in the index template as a string
+    helpers.getTemplate('accountEdit', templateData, (err, str) => {
+      if (!err && str) {
+        //Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, (err, str) => {
+          if (!err && str) {
+            //return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
 //Favicon
 handlers.favicon = (data, callback) => {
   //Reject an request that is not GET
